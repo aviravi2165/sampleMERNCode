7c276f2594ab5830c7b90638710f499d23dc0eed
avi.ravi2165 <avi.ravi2165@gmail.com>
2024-09-14 18:35:04 +0530

    cors added

--- backend/app.ts
+++ backend/app.ts
@@ -4,13 +4,13 @@ import cors from "cors";
 require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 5000;
+const corsAddress = process.env.PORT || "http://localhost:3000";
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with your allowed origin
+  origin: corsAddress, // Replace with your allowed origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
-
-const port = process.env.PORT || 5000;
 
 app.use(cors(corsOptions));
 
