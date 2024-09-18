f568702b07ffeb90dd8d9205c3e0f3abf800b4e8
aviravi2165 <avi.ravi2165@gmail.com>
2024-09-18 18:07:34 +0530

    For local

--- backend/app.ts
+++ backend/app.ts
@@ -5,7 +5,8 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const corsAddress = process.env.PORT || "http://localhost:3000";
+const corsAddress = process.env.CORS;
+console.log("corsAddress", corsAddress);
 
 const corsOptions = {
   origin: corsAddress, // Replace with your allowed origin
